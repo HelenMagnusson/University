0adb5e16937fa99529ee90df1872e787f4827727
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-09-01 10:54:52 +0200

    Entites

---
+++ University/Models/Entities/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Models.Entities
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int Grade { get; set; }
+    }
+}
---
+++ University/Models/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Models.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Avatar { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
